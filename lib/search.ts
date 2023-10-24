26a0492652d7284209088df8ae12776bf91704ff
fansi-sifan <sifan1121@gmail.com>
2023-10-23 21:31:36 -0700

    increase similariy threshold

--- lib/search.ts
+++ lib/search.ts
@@ -62,7 +62,7 @@ export async function search(query: string)  {
     
    
     const retriever = ScoreThresholdRetriever.fromVectorStore(vectorStore, {
-      minSimilarityScore: 0.7, // Finds results with at least this similarity score
+      minSimilarityScore: 0.8, // Finds results with at least this similarity score
       maxK: 10, // The maximum K value to use. Use it based to your chunk size to make sure you don't run out of tokens
       kIncrement: 2, // How much to increase K by each time. It'll fetch N results, then N + kIncrement, then N + kIncrement * 2, etc.
     });
